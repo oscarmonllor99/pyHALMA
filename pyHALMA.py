Fixing potential calculation for 1 DM species. Checkpoint from file properly working. Calipso.make_light non parallel (undesired bug).

pyHALMA.py
@@ -687,6 +687,13 @@ for it_count, iteration in enumerate(range(FIRST, LAST+STEP, STEP)):
                                                                     output_position=True, output_velocity=False, output_mass=True, read_region=read_region)
                     print('     Done')
 
+
+            # IDENTIFY DARK MATTER SPECIES IF FIRST ITERATION
+            if it_count == 0:
+                num_dm_species = len(np.unique(masclet_dm_data[3]))
+                print('     Number of DM species:', num_dm_species)
+
+
             # BUILD DARK MATTER KDTREE FOR POT. ENERGY PURPOSES
             if DM_FLAG or RPS_FLAG or POT_ENERGY_FLAG:
                 ##############################################
@@ -697,7 +704,6 @@ for it_count, iteration in enumerate(range(FIRST, LAST+STEP, STEP)):
                 print('     Time to build DM kdtree:', time.time()-t0, 's')
                 ##############################################
                 print()
-
 
             ###########################################
             ###########################################
@@ -1024,7 +1030,7 @@ for it_count, iteration in enumerate(range(FIRST, LAST+STEP, STEP)):
                                                                 st_kdtree, dm_kdtree,
                                                                 cx, cy, cz, POT_RADIUS, rho_B
                                                                 )
-                    
+
                 # RPS EFFECTS
                 if RPS_FLAG:
                     (
@@ -1039,7 +1045,7 @@ for it_count, iteration in enumerate(range(FIRST, LAST+STEP, STEP)):
                                                                             st_x_in, st_y_in, st_z_in, st_mass_in,
                                                                             velocities_x[ihal], velocities_y[ihal], 
                                                                             velocities_z[ihal], BRUTE_FORCE_LIM,
-                                                                            mass_dm_part    
+                                                                            mass_dm_part, num_dm_species    
                                                                         )
                     
 
@@ -1434,17 +1440,6 @@ for it_count, iteration in enumerate(range(FIRST, LAST+STEP, STEP)):
                 halo = new_groups[argsort_part[isort_part]]
                 part_ih_before[halo] = isort_part + 1
 
-            #Save checkpoint
-            checkpoint_file = os.path.join(PYHALMA_OUTPUT, 'HALMA_checkpoint')
-            info_checkpoint = np.array([iteration, cosmo_time, len(new_groups)])
-            np.savez(checkpoint_file, 
-                info = info_checkpoint,
-                omm = omm, npart = num_particles, oripas_before = oripas_before_save,
-                part_ih_before = part_ih_before, part_oripas_before = part_oripas_before, part_insitu_before = part_insitu_before,
-                part_ih_before2 = part_ih_before2, part_oripas_before2 = part_oripas_before2, part_insitu_before2 = part_insitu_before2,
-                pro1_before2 = pro1_before2)
-            ##########################################
-
             ############################################################################################################
             # CALIPSO BLOCK
             ############################################################################################################
@@ -1535,6 +1530,7 @@ for it_count, iteration in enumerate(range(FIRST, LAST+STEP, STEP)):
                     grid_edges = np.arange(-rmax[ihal]*1e3 - res, rmax[ihal]*1e3 + res, res)
                     grid_centers = (grid_edges[1:] + grid_edges[:-1]) / 2
                     ncell = len(grid_centers)  # number of cells in each direction
+
                     which_cell_x, which_cell_y, which_cell_z = pycalipso.put_particles_in_grid(grid_centers, x, y, z)
 
                     # finner grid to interpolate
@@ -1571,7 +1567,7 @@ for it_count, iteration in enumerate(range(FIRST, LAST+STEP, STEP)):
                         gr, ur,
                         sbf, magf, fluxf    )  = pycalipso.main(calipso_input, star_particle_data, 
                                                             ncell, vel_LOS, tam_i, tam_j, effective_radius, rete)
-
+                    
                     # SÉRSIC INDEX WITH LIGHT (g filter SDSS)
                     # FIRST: LINEAR INTERPOLATION OF THE FLUX IN THE GRID
 
@@ -1872,6 +1868,18 @@ for it_count, iteration in enumerate(range(FIRST, LAST+STEP, STEP)):
         all_particles_in_haloes = np.concatenate(new_groups)
         np.save(PYHALMA_OUTPUT + '/stellar_particles'+string_it+'.npy', all_particles_in_haloes)
     ###########################################
+
+    #save checkpoint ##########################
+    checkpoint_file = os.path.join(PYHALMA_OUTPUT, 'HALMA_checkpoint')
+    info_checkpoint = np.array([iteration, cosmo_time, len(new_groups)])
+    np.savez(checkpoint_file, 
+        info = info_checkpoint,
+        omm = omm, npart = num_particles, oripas_before = oripas_before_save,
+        part_ih_before = part_ih_before, part_oripas_before = part_oripas_before, part_insitu_before = part_insitu_before,
+        part_ih_before2 = part_ih_before2, part_oripas_before2 = part_oripas_before2, part_insitu_before2 = part_insitu_before2,
+        pro1_before2 = pro1_before2)
+    ##########################################
+
 
 ###########################################
 #Clean all variables

python_scripts/halo_gas.py
@@ -284,7 +284,7 @@ def st_gas_dm_particles_inside(rete, L, ncoarse, grid_data,
 
 def RPS(gas_x, gas_y, gas_z, gas_vx, gas_vy, gas_vz, gas_mass, gas_temp, 
         dm_x, dm_y, dm_z, dm_mass, st_x, st_y, st_z, st_mass, vx, vy, vz, BRUTE_FORCE_LIM,
-        mass_dm_part):
+        mass_dm_part, num_dm_species):
     
     ##################################################################################
     # This routine aims to calculate the bound/unbound mass gas fraction of each halo
@@ -321,6 +321,7 @@ def RPS(gas_x, gas_y, gas_z, gas_vx, gas_vy, gas_vz, gas_mass, gas_temp,
         binding_energy += binding_energy_gas
         
     elif ngas > 0:
+
         binding_energy_gas = brute_force_binding_energy_fortran(
                             gas_mass, gas_x, gas_y, gas_z, gas_x, gas_y, gas_z
                             )
@@ -330,62 +331,94 @@ def RPS(gas_x, gas_y, gas_z, gas_vx, gas_vy, gas_vz, gas_mass, gas_temp,
 
     ##########################################
     # POTENTIAL ENERGY DUE TO DARK MATTER
-    #TAKE INTO ACCOUNT ALWAYS ALL l<=1 PARTICLES, AS THEY ARE
-    #THE MOST MASSIVE ONES
-    mass_l0 = mass_dm_part
-    mass_l1 = mass_dm_part/8
-    mass_l2 = mass_dm_part/8**2
-    mass_l3 = mass_dm_part/8**3
-    mass_l4 = mass_dm_part/8**4
 
-    # l = 0 and l = 1
-    condition_l01 = dm_mass >= 0.9*mass_l1
-    dm_x_mandatory = dm_x[condition_l01]
-    dm_y_mandatory = dm_y[condition_l01]
-    dm_z_mandatory = dm_z[condition_l01]
-    dm_mass_mandatory = dm_mass[condition_l01]
+    # If several DM species exists (low number of massive and high number of low mass particles)
+    # MASCLET OLD WAY
+    if num_dm_species > 1:
+        #TAKE INTO ACCOUNT ALWAYS ALL l<=1 PARTICLES, AS THEY ARE
+        #THE MOST MASSIVE ONES
+        mass_l0 = mass_dm_part
+        mass_l1 = mass_dm_part/8
+        mass_l2 = mass_dm_part/8**2
+        mass_l3 = mass_dm_part/8**3
+        mass_l4 = mass_dm_part/8**4
 
-    if np.count_nonzero(condition_l01) > 0:
-        binding_energy_dm = brute_force_binding_energy_fortran(
-                                dm_mass_mandatory, dm_x_mandatory, 
-                                dm_y_mandatory, dm_z_mandatory, gas_x, gas_y, gas_z
-                                )
-        
-        binding_energy += binding_energy_dm
+        # l = 0 and l = 1
+        condition_l01 = dm_mass >= 0.9*mass_l1
+        dm_x_mandatory = dm_x[condition_l01]
+        dm_y_mandatory = dm_y[condition_l01]
+        dm_z_mandatory = dm_z[condition_l01]
+        dm_mass_mandatory = dm_mass[condition_l01]
 
-    # l = 2, l = 3 and l = 4
-    condition_other = np.logical_not(condition_l01)
-    dm_x_other = dm_x[condition_other]
-    dm_y_other = dm_y[condition_other]
-    dm_z_other = dm_z[condition_other]
-    dm_mass_other = dm_mass[condition_other]
+        if np.count_nonzero(condition_l01) > 0:
+            binding_energy_dm = brute_force_binding_energy_fortran(
+                                    dm_mass_mandatory, dm_x_mandatory, 
+                                    dm_y_mandatory, dm_z_mandatory, gas_x, gas_y, gas_z
+                                    )
+            
+            binding_energy += binding_energy_dm
 
-    #Sampling the DM particles with replacement
-    ndm_other = len(dm_x_other)
-    if ndm_other > BRUTE_FORCE_LIM:
-        nsample_dm = np.max([BRUTE_FORCE_LIM, int(0.01*ndm_other)])
-        dm_part_list = np.arange(ndm_other)
-        sample_dm = np.random.choice(dm_part_list, nsample_dm, replace=True)
-        dm_x_other = dm_x_other[sample_dm]
-        dm_y_other = dm_y_other[sample_dm]
-        dm_z_other = dm_z_other[sample_dm]
-        dm_mass_other = dm_mass_other[sample_dm]
-        
-        binding_energy_dm = brute_force_binding_energy_fortran(
-                                dm_mass_other, dm_x_other,
+        # l = 2, l = 3 and l = 4
+        condition_other = np.logical_not(condition_l01)
+        dm_x_other = dm_x[condition_other]
+        dm_y_other = dm_y[condition_other]
+        dm_z_other = dm_z[condition_other]
+        dm_mass_other = dm_mass[condition_other]
+
+        #Sampling the DM particles with replacement
+        ndm_other = len(dm_x_other)
+        if ndm_other > BRUTE_FORCE_LIM:
+            nsample_dm = np.max([BRUTE_FORCE_LIM, int(0.01*ndm_other)])
+            dm_part_list = np.arange(ndm_other)
+            sample_dm = np.random.choice(dm_part_list, nsample_dm, replace=True)
+            dm_x_other = dm_x_other[sample_dm]
+            dm_y_other = dm_y_other[sample_dm]
+            dm_z_other = dm_z_other[sample_dm]
+            dm_mass_other = dm_mass_other[sample_dm]
+            
+            binding_energy_dm = brute_force_binding_energy_fortran(
+                                    dm_mass_other, dm_x_other,
+                                    dm_y_other, dm_z_other, gas_x, gas_y, gas_z
+                                    )
+                
+            binding_energy_dm = binding_energy_dm * ndm_other / nsample_dm
+            binding_energy += binding_energy_dm 
+
+        elif ndm_other > 0:
+            binding_energy_dm = brute_force_binding_energy_fortran(
+                                dm_mass_other, dm_x_other, 
                                 dm_y_other, dm_z_other, gas_x, gas_y, gas_z
                                 )
             
-        binding_energy_dm = binding_energy_dm * ndm_other / nsample_dm
-        binding_energy += binding_energy_dm 
+            binding_energy += binding_energy_dm
+ 
+    # if only 1 dm species exists, sample all particles
+    else:
+        ndm = len(dm_x)
+        if ndm > BRUTE_FORCE_LIM:
+            nsample_dm = np.max([BRUTE_FORCE_LIM, int(0.01*ndm)])
+            dm_part_list = np.arange(ndm)
+            sample_dm = np.random.choice(dm_part_list, nsample_dm, replace=True)
+            dm_x_sample = dm_x[sample_dm]
+            dm_y_sample = dm_y[sample_dm]
+            dm_z_sample = dm_z[sample_dm]
+            dm_mass_sample = dm_mass[sample_dm]
+            
+            binding_energy_dm = brute_force_binding_energy_fortran(
+                                    dm_mass_sample, dm_x_sample,
+                                    dm_y_sample, dm_z_sample, gas_x, gas_y, gas_z
+                                    )
+                
+            binding_energy_dm = binding_energy_dm * ndm / nsample_dm
+            binding_energy += binding_energy_dm
 
-    elif ndm_other > 0:
-        binding_energy_dm = brute_force_binding_energy_fortran(
-                            dm_mass_other, dm_x_other, 
-                            dm_y_other, dm_z_other, gas_x, gas_y, gas_z
-                            )
-        
-        binding_energy += binding_energy_dm
+        elif ndm > 0:
+            binding_energy_dm = brute_force_binding_energy_fortran(
+                                dm_mass, dm_x, 
+                                dm_y, dm_z, gas_x, gas_y, gas_z
+                                )
+            
+            binding_energy += binding_energy_dm
 
     ################################################
 

python_scripts/pycalipso.py
@@ -387,6 +387,7 @@ def put_particles_in_grid(grid_centers, x, y, z):
     return which_cell_x, which_cell_y, which_cell_z
 
 
+
 def main(calipso_input, star_particle_data, ncell, vel_LOS, tam_i, tam_j, effective_radius, rete):
 
     ####### INPUT FORMAT
@@ -405,6 +406,7 @@ def main(calipso_input, star_particle_data, ncell, vel_LOS, tam_i, tam_j, effect
     [npart, mass, met, age] = star_particle_data
 
     # Finding fluxes in each cell and luminosity weighted quantities
+
     (
      flux_cell, flux_cell_sig, fluxtot, vell_malla, 
      sigl_malla, lum_malla, twl, Zwl, vell2_malla
@@ -477,7 +479,7 @@ def main(calipso_input, star_particle_data, ncell, vel_LOS, tam_i, tam_j, effect
                                                     W_FILTERS, RESPONSE_FILTERS,
                                                     dlum, zeta, ncell, ncell, flux_cell, area_arc)
 
-    
+
     sbf[fluxf==0.] = np.nan # if flux is zero, magnitudes are undefined
     magf[fluxf==0.] = np.nan
 